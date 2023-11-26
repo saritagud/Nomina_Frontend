7520dbd96a1b77d409ac9e100d155e1ecc052dd0
saritagud <gudinop864@gmail.com>
2023-11-25 23:23:23 -0400

    agregado boton en la vista de empresas

--- src/Components/Companies.jsx
+++ src/Components/Companies.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FaEllipsisV } from 'react-icons/fa';
 
 export function Companies() {
   const [company, setCompany] = useState([]);
@@ -49,9 +51,40 @@ export function Companies() {
                         {companys.name}
                       </td>
                       <td className="p-4 text-lg">{companys.type}</td>
-                      <button className="bg-azulClaro p-2 mt-2 text-white w-1/2 rounded-md">
-                        Seleccionar
-                      </button>
+                      <td className="relative p-4 text-lg rounded-r-2xl">
+                        <input
+                          type="checkbox"
+                          name={`action${companys.id}`}
+                          id={`action${companys.id}`}
+                          className="hidden peer/action"
+                        />
+                        <label
+                          htmlFor={`action${companys.id}`}
+                          className="cursor-pointer"
+                        >
+                          <FaEllipsisV />
+                        </label>
+                        <div className="hidden absolute peer-checked/action:flex gap-4 right-52 top-1/2 transform -translate-y-1/2 bg-grisClaro shadow-right-dark p-5 rounded-lg z-10">
+                          <Link
+                            className="text-white w-28 rounded-md bg-azulClaro px-2 py-1 font-semibold text-center"
+                            to={""}
+                          >
+                            Ver
+                          </Link>
+                          <button
+                            className="text-white w-28 rounded-md bg-azulClaro px-2 py-1 font-semibold"
+                            onClick={() => {}}
+                          >
+                            Editar
+                          </button>
+                          <button
+                            className="text-white w-28 rounded-md bg-red-600 px-2 py-1 font-semibold"
+                            onClick={() => {}}
+                          >
+                            Eliminar
+                          </button>
+                        </div>
+                      </td>
                     </tr>
                   </tbody>
                 ))}
