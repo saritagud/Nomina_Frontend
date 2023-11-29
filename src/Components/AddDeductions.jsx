a5679ae428375ca71999cadd39e49d663a59949a
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-11-29 13:52:11 -0400

    Vista y modal para gestionar deducciones

--- src/Components/AddDeductions.jsx
+++ src/Components/AddDeductions.jsx
@@ -1,107 +1,237 @@
+import { useEffect, useState } from "react";
 import { BiArrowBack } from "react-icons/bi";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { userRoles } from "../logic/constantes";
-import { authComponent } from "../logic/authComponent";
+import { createDeductionName, getAllDeductionName } from "../services/deductions";
 
-export function AddDeductions({ setModalDeduction }) {
-  const navegar = useNavigate();
-  const [name, setName] = useState("");
-  const [amount, setAmount] = useState("");
-  const [taxInformation, setTaxInformation] = useState("");
-  const token = JSON.parse(localStorage.getItem("token"));
+export function AddDeductions({ dataEdit = null, setStateModal, confirm }) {
+  const token = JSON.parse(localStorage.getItem("token"))
+  const [nameCreated, setNameCreated] = useState(null)
+  const [selectedOther, setSelectedOther] = useState(false)
+  const [deductionName, setDeductionName] = useState('')
+  const [deductionsNames, setDeductionsNames] = useState([])
+  const [deductionSelected, setDeductionSelected] = useState('0')
+  const [deductionData, setDeductionData] = useState({
+    percentage: '',
+    application: '0',
+    state: '0'
+  })
 
-  const { Admin } = userRoles;
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    // Comprueba que el componente siga teniendo una sesion activa y el rol sea permitido
-    const auth = authComponent([Admin]);
-    if (!auth) return navegar("/admin");
-
-    if ((!name, !amount, !taxInformation)) {
-      console.log("Debes rellenar todos los campos");
-    } else {
-      const data = {
-        name: name,
-        amount: amount,
-        taxInformation: taxInformation,
-      };
-
-      fetch("http://localhost:3000/deduction/create-deduction", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("Success:", data.newApartment);
-          if (data.newApartment) {
-            setModalDeduction(false);
-            navegar("/admin");
-          } else {
-            console.log("Error:", data.error);
-          }
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
+  useEffect(() => {
+    const getDeduc = async () => {
+      const res = await getAllDeductionName(token)
+      if (res?.deductionsName) {
+        setDeductionsNames(res.deductionsName)
+      }
     }
-  };
+    getDeduc()
+    if (dataEdit) {
+      setDeductionData({
+        percentage: dataEdit.percentage.toString(),
+        application: dataEdit.application,
+        state: dataEdit.state,
+      })
+      setDeductionSelected(dataEdit.deductionName.id)
+    }
+  }, [])
+  
+  
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    if (name === 'deduction') {
+      setDeductionSelected(value)
+      if (value === 'other') {
+        setSelectedOther(true)
+      } else if (selectedOther === true) {
+        setSelectedOther(false)
+        setDeductionName('')
+        setNameCreated(null)
+      }
+    } else {
+      setDeductionData({
+        ...deductionData,
+        [name]: value
+      })
+    }
+  }
+
+  const handleCreateName = async e => {
+    e.preventDefault()
+    const res = await createDeductionName(token, {name: deductionName})
+    if (res?.name) {
+      // console.log('Success: ', res.name);
+      setNameCreated(res)
+    } else {
+      console.error(res)
+    }
+  }
+
+  const handleSubmit = async () => {
+    let data = {}
+
+    if (deductionSelected === '0') return console.error('Debes seleccionar el tipo de deduccion')
+    if (deductionSelected === 'other') {
+      if (deductionName === '') return console.error('Debes ingresar el nombre de la deduccion')
+    }
+    if (deductionData.percentage === '') return console.error('Debes ingresar el Monto de la deduccion')
+    if (deductionData.application === '0') return console.error('Debes seleccionar la Aplicacion de la deduccion')
+    if (deductionData.state === '0') return console.error('Debes seleccionar el Estado de la deduccion')
+
+    if (dataEdit) {
+      if (deductionData.percentage !== dataEdit.percentage)
+        data = {
+          ...data,
+          percentage: parseFloat(deductionData.percentage),
+        };
+      if (deductionData.application !== dataEdit.application)
+        data = {
+          ...data,
+          application: deductionData.application,
+        };
+      if (deductionData.state !== dataEdit.state)
+        data = {
+          ...data,
+          state: deductionData.state,
+        };
+
+      confirm(data, dataEdit.id)
+    } else {
+      data = {
+        ...deductionData,
+        percentage: parseFloat(deductionData.percentage),
+      };
+      // console.log(data);
+      if (nameCreated?.id) {
+        confirm(data, nameCreated.id)
+      } else {
+        confirm(data, deductionSelected)
+      }
+    }
+  }
   return (
-    <>
-      <section className="absolute top-0 left-0 bottom-0 right-0 bg-grisClaro flex flex-col justify-center items-center gap-10">
-        <BiArrowBack
-          className="absolute top-2 left-3 z-10 text-3xl cursor-pointer"
-          onClick={() => setModalDeduction(false)}
-        ></BiArrowBack>
-        <h2 className="text-3xl text-start ">Crear Departamento</h2>
-        <form className="flex flex-col gap-5 " onSubmit={handleSubmit}>
-          <label htmlFor="name" className="text-xl">
-            Nombre de la deduccion
-          </label>
-          <input
-            type="text"
-            value={name}
-            id="name"
-            onChange={(e) => setName(e.target.value)}
-            className="bg-azulClaro px-3 py-2 rounded-md placeholder-grisClaro text-grisClaro outline-none w-80"
-            placeholder="Ingresa el nombre de tu empresa"
-          />
-
-          <label htmlFor="name" className="text-xl">
-            Monto
-          </label>
-          <input
-            type="text"
-            value={amount}
-            id="name"
-            onChange={(e) => setAmount(e.target.value)}
-            className="bg-azulClaro px-3 py-2 rounded-md placeholder-grisClaro text-grisClaro outline-none w-80"
-            placeholder="Ingresa el nombre de tu empresa"
-          />
-
-          <label htmlFor="name" className="text-xl">
-            Descripcion
-          </label>
-          <input
-            type="text"
-            value={taxInformation}
-            id="name"
-            onChange={(e) => setTaxInformation(e.target.value)}
-            className="bg-azulClaro px-3 py-2 rounded-md placeholder-grisClaro text-grisClaro outline-none w-80"
-            placeholder="Ingresa el nombre de tu empresa"
-          />
-
-          <button className="bg-azulOscuro mx-auto px-3 py-2 font-bold text-grisClaro outline-none rounded-md mt-10">
-            Crear Departamento
-          </button>
-        </form>
-      </section>
-    </>
-  );
+    
+    <section className="fixed top-0 left-0 bottom-0 right-0 bg-grisClaro flex flex-col items-center justify-center z-20 min-h-screen gap-10">
+      <h1 className="text-4xl font-bold">
+        {dataEdit ? 'Editar datos de la deduccion' : 'Agrega una deduccion'}
+      </h1>
+      <BiArrowBack
+        className="absolute top-2 left-3 z-10 text-3xl cursor-pointer"
+        onClick={() => setStateModal(false)}
+      />
+      <form
+        className="flex flex-col gap-10"
+        onSubmit={handleSubmit}
+      >
+        <section className="flex gap-10">
+          <div className="flex flex-col gap-1">
+            <label htmlFor="deduction" className="text-xl">
+              Deduccion
+            </label>
+            <select
+              name="deduction"
+              id="deduction"
+              className="bg-azulClaro px-3 py-2 rounded-md text-grisClaro outline-none w-80"
+              value={deductionSelected}
+              onChange={handleChange}
+              disabled={!!dataEdit}
+            >
+              <option value="0" disabled>
+                -- Elegir deduccion --
+              </option>
+              {deductionsNames.map((deduction) => (
+                <option key={deduction.id} value={deduction.id}>
+                  {deduction.name}
+                </option>
+              ))}
+              <option value="other">
+                Otro
+              </option>
+            </select>
+          </div>
+          {selectedOther && (
+            <>
+              <div className="flex flex-col gap-1">
+                <label htmlFor="deductionName" className="text-xl">
+                  Nombre
+                </label>
+                <input
+                  type="text"
+                  name="deductionName"
+                  id="deductionName"
+                  autoFocus
+                  className="bg-azulClaro px-3 py-2 rounded-md placeholder-grisClaro text-grisClaro outline-none w-80"
+                  placeholder="Ingresa el nombre"
+                  value={deductionName}
+                  onChange={e => setDeductionName(e.target.value)}
+                />
+              </div>
+              <button 
+                className="bg-azulOscuro mx-auto mt-auto px-3 py-2 font-bold text-grisClaro outline-none rounded-md"
+                onClick={handleCreateName}>
+                Crear nombre
+              </button>
+            </>
+          )}
+        </section>
+        <div className="flex gap-10">
+          <div className="flex flex-col gap-1">
+            <label htmlFor="percentage" className="text-xl">
+              Porcentaje{" "}
+              <span className="text-sm">
+                {'(Para centimos usar punto ".")'}
+              </span>
+            </label>
+            <input
+              type="text"
+              name="percentage"
+              id="percentage"
+              className="bg-azulClaro px-3 py-2 rounded-md placeholder-grisClaro text-grisClaro outline-none w-80"
+              value={deductionData.percentage}
+              placeholder="Ingresa un porcentaje ( 5.5 )"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="application" className="text-xl">
+              Aplicacion
+            </label>
+            <select
+              name="application"
+              id="application"
+              className="bg-azulClaro px-3 py-2 rounded-md placeholder-grisClaro text-grisClaro outline-none w-80"
+              value={deductionData.application}
+              onChange={handleChange}
+            >
+              <option value="0" disabled>
+                -- Seleccionar --
+              </option>
+              <option value="Aplica">Aplica</option>
+              <option value="No Aplica">No Aplica</option>
+            </select>
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="state" className="text-xl">
+              Estado
+            </label>
+            <select
+              name="state"
+              id="state"
+              className="bg-azulClaro px-3 py-2 rounded-md placeholder-grisClaro text-grisClaro outline-none w-80"
+              value={deductionData.state}
+              onChange={handleChange}
+            >
+              <option value="0" disabled>
+                -- Seleccionar --
+              </option>
+              <option value="Activo">Activo</option>
+              <option value="Inactivo">Inactivo</option>
+            </select>
+          </div>
+        </div>
+      </form>
+      <button 
+        className="bg-azulOscuro mx-auto mt-5 px-3 py-2 font-bold text-grisClaro outline-none rounded-md"
+        onClick={() => handleSubmit()}>
+        {dataEdit ? 'Guardar Cambios' : 'Agregar Deduccion'}
+      </button>
+    </section>
+  )
 }
--- src/Components/Deductions.jsx
+++ src/Components/Deductions.jsx
@@ -1,5 +1,144 @@
-export function Deductions(){
-    return(
-        <></>
-    )
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BiArrowBack } from "react-icons/bi";
+import { FaEllipsisV } from "react-icons/fa";
+import { createDeductionData, deleteEmployee, editDeductionData, getAllDeductions } from "../services/deductions";
+import { addItemToState, deleteItemFromState, udpateItemFromState } from "../logic/functions";
+import ModalDelete from "./ModalDelete";
+import { AddDeductions } from "./AddDeductions";
+
+export function Deductions() {
+  const token = JSON.parse(localStorage.getItem("token"))
+  const navegar = useNavigate()
+  const { emploID } = useParams()
+  const [deductions, setDeductions] = useState([])
+  const [deductionSelect, setDeductionSelect] = useState(null)
+  const [modalAddDeduc, setModalAddDeduc] = useState(false)
+  const [modalDelete, setModalDelete] = useState(false)
+  const [modalEdit, setModalEdit] = useState(false)
+
+  useEffect(() => {
+    const getDeduc = async () => {
+      const res = await getAllDeductions(token, emploID)
+      if (res?.deductions) {
+        setDeductions(res.deductions)
+      }
+    }
+    getDeduc()
+  }, [])
+
+  
+  const confirmDelete = async () => {
+    const res = await deleteEmployee(token, deductionSelect, emploID)
+    if (res.message) {
+      const newState = deleteItemFromState(deductionSelect, [...deductions])
+      setDeductions(newState)
+    } else {
+      console.error(res)
+    }
+  }
+
+  const confirmAddDeduction = async (data, deductionId) => {
+    const res = await createDeductionData(token, emploID, deductionId ,data)
+    if (res) {
+    //   console.log('Success: ', res);
+      const newList = addItemToState(res, [...deductions])
+      setDeductions(newList)
+      setModalAddDeduc(false)
+    } else {
+      console.error(res)
+    }
+  }
+
+  const confirmEditDeduction = async (data, deductionId) => {
+    const res = await editDeductionData(token, emploID, deductionId ,data)
+    if (res) {
+    //   console.log('Success: ', res);
+      const newList = udpateItemFromState(res, [...deductions])
+      setDeductions(newList)
+      setModalEdit(false)
+    } else {
+      console.error(res)
+    }
+  }
+
+  return (
+    <div className="h-full">
+      <main className="relative px-10 py-5 flex flex-col gap-10 w-full">
+        <section className="flex justify-between items-center gap-5">
+          <div className="flex items-center gap-5">
+            <BiArrowBack
+              className="text-3xl cursor-pointer translate-y-[2px]"
+              onClick={() => navegar(`/empleado/${emploID}`)}
+            />
+            <h1 className="text-3xl">Deducciones</h1>
+          </div>
+          <div>
+            <button
+              className="bg-azulClaro p-2 pr-6 pl-6 text-white rounded-md font-semibold"
+              onClick={() => setModalAddDeduc(true)}
+            >
+              Agregar
+            </button>
+          </div>
+        </section>
+        {deductions.length > 0 && (
+          <section className="">
+            <table className="w-full bg-grisClaro rounded-md shadow-right-dark px-4 border-separate border-spacing-0 border-spacing-y-4 border-2 border-grisOscuro">
+              <thead className="px-5">
+                <tr>
+                  <th className="p-4 text-lg text-start">Tipo</th>
+                  <th className="p-4 text-lg text-start">Porcentaje</th>
+                  <th className="p-4 text-lg text-start">Aplicacion</th>
+                  <th className="p-4 text-lg text-start">Estado</th>
+                </tr>
+              </thead>
+              <tbody className="px-5">
+                {deductions.map((deduction) => (
+                  <tr className="bg-grisOscuro hover:bg-blue-300" key={deduction.id}>
+                    <td className="p-4 text-lg rounded-l-2xl">{deduction.deductionName.name}</td>
+                    <td className="p-4 text-lg">{deduction.percentage} %</td>
+                    <td className="p-4 text-lg">{deduction.application}</td>
+                    <td className="p-4 text-lg">{deduction.state}</td>
+                    <td className="relative p-4 text-lg rounded-r-2xl">
+                      <input type="checkbox" name={`action${deduction.id}`} id={`action${deduction.id}`} className="hidden peer/action"/>
+                      <label htmlFor={`action${deduction.id}`} className="cursor-pointer">
+                        <FaEllipsisV/>
+                      </label>
+                      <div className="hidden absolute peer-checked/action:flex gap-4 right-28 top-1/2 transform -translate-y-1/2 bg-grisClaro shadow-right-dark p-5 rounded-lg z-10">
+                        <button className="text-white w-28 rounded-md bg-azulClaro px-2 py-1 font-semibold" onClick={() => {
+                          setDeductionSelect(deduction)
+                          setModalEdit(true)
+                        }}>Editar</button>
+                        <button className="text-white w-28 rounded-md bg-red-600 px-2 py-1 font-semibold" onClick={() => {
+                          setDeductionSelect(deduction.id)
+                          setModalDelete(true)
+                        }}>Eliminar</button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </section>
+        )}
+        {modalAddDeduc && (
+          <AddDeductions
+            setStateModal={setModalAddDeduc}
+            confirm={confirmAddDeduction}
+          />
+        )}
+        {modalDelete && (
+          <ModalDelete peticion={confirmDelete} setStateModal={setModalDelete}/>
+        )}
+        {modalEdit && (
+          <AddDeductions
+            dataEdit={deductionSelect}
+            setStateModal={setModalEdit}
+            confirm={confirmEditDeduction}
+          />
+        )}
+      </main>
+    </div>
+  )
 }
