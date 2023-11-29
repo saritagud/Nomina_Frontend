f7c24e6be88158e60abdf29757060d9e41493a16
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-11-29 13:54:40 -0400

    Tabla condicionada

--- src/Components/Employees.jsx
+++ src/Components/Employees.jsx
@@ -63,40 +63,41 @@ export function Employees() {
             </button>
           </div>
         </div>
-        <table className="bg-grisClaro rounded-md shadow-right-dark px-4 border-separate border-spacing-0 border-spacing-y-4 border-2 border-grisOscuro">
-          <thead className="px-5">
-            <tr>
-              <th className="p-4 text-lg text-start">Empleado</th>
-              <th className="p-4 text-lg text-start">Identificación</th>
-              <th className="p-4 text-lg text-start">Cargo</th>
-              <th className="p-4 text-lg text-start">Departamento</th>
-            </tr>
-          </thead>
-          <tbody className="px-5">
-            {employees.map((employee) => (
-              <tr className="bg-grisOscuro hover:bg-blue-300" key={employee.id}>
-                <td className="p-4 text-lg rounded-l-2xl">{employee.name}</td>
-                <td className="p-4 text-lg">{employee.identityCard}</td>
-                <td className="p-4 text-lg">{employee.charge}</td>
-                <td className="p-4 text-lg">{employee.departamentId}</td>
-                <td className="p-4 text-lg flex gap-4"></td>
-                <td className="relative p-4 text-lg rounded-r-2xl">
-                  <input type="checkbox" name={`action${employee.id}`} id={`action${employee.id}`} className="hidden peer/action"/>
-                  <label htmlFor={`action${employee.id}`} className="cursor-pointer">
-                    <FaEllipsisV/>
-                  </label>
-                  <div className="hidden absolute peer-checked/action:flex gap-4 right-20 top-1/2 transform -translate-y-1/2 bg-grisClaro shadow-right-dark p-5 rounded-lg z-10">
-                    <Link to={`/empleado/${employee.id}`} className="text-white w-28 text-center rounded-md bg-azulClaro px-2 py-1 font-semibold">Ver</Link>
-                    <button className="text-white w-28 rounded-md bg-red-600 px-2 py-1 font-semibold" onClick={() => {
-                      setEmployeeSelect(employee.id)
-                      setModalDelete(true)
-                    }}>Eliminar</button>
-                  </div>
-                </td>
+        {employees.length > 0 && (
+          <table className="bg-grisClaro rounded-md shadow-right-dark px-4 border-separate border-spacing-0 border-spacing-y-4 border-2 border-grisOscuro">
+            <thead className="px-5">
+              <tr>
+                <th className="p-4 text-lg text-start">Empleado</th>
+                <th className="p-4 text-lg text-start">Identificación</th>
+                <th className="p-4 text-lg text-start">Cargo</th>
+                <th className="p-4 text-lg text-start">Departamento</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="px-5">
+              {employees.map((employee) => (
+                <tr className="bg-grisOscuro hover:bg-blue-300" key={employee.id}>
+                  <td className="p-4 text-lg rounded-l-2xl">{employee.name}</td>
+                  <td className="p-4 text-lg">{employee.identityCard}</td>
+                  <td className="p-4 text-lg">{employee.charge}</td>
+                  <td className="p-4 text-lg">{employee.departamentId}</td>
+                  <td className="relative p-4 text-lg rounded-r-2xl">
+                    <input type="checkbox" name={`action${employee.id}`} id={`action${employee.id}`} className="hidden peer/action"/>
+                    <label htmlFor={`action${employee.id}`} className="cursor-pointer">
+                      <FaEllipsisV/>
+                    </label>
+                    <div className="hidden absolute peer-checked/action:flex gap-4 right-20 top-1/2 transform -translate-y-1/2 bg-grisClaro shadow-right-dark p-5 rounded-lg z-10">
+                      <Link to={`/empleado/${employee.id}`} className="text-white w-28 text-center rounded-md bg-azulClaro px-2 py-1 font-semibold">Ver</Link>
+                      <button className="text-white w-28 rounded-md bg-red-600 px-2 py-1 font-semibold" onClick={() => {
+                        setEmployeeSelect(employee.id)
+                        setModalDelete(true)
+                      }}>Eliminar</button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
         {modalDelete && (
           <ModalDelete peticion={confirmDelete} setStateModal={setModalDelete}/>
         )}
