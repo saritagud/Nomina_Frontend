646b269ae1426d5e46f339cebaf84aa560b23c49
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-12-05 21:18:13 -0400

    Pre-nomina modificada

--- src/Components/PrePayroll.jsx
+++ src/Components/PrePayroll.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { FaAngleDown, FaEllipsisV } from "react-icons/fa"
 import ModalDelete from "./ModalDelete"
-import { paymentPeriod } from "../logic/constantes"
+import { paymentPeriod as paymentPeriods, statesPayroll } from "../logic/constantes"
 import {
   formatMonth,
   formatRangeFirst,
@@ -12,26 +12,33 @@ import {
 } from "../logic/functions"
 import { getAllPerceptionsName } from "../services/perceptions"
 import { getAllDeductionName } from "../services/deductions"
+import { deletePayroll, deleteUserOfPayroll, updateStatePayroll } from "../services/payroll"
 
 export function PrePayroll() {
-  const { Semanal, PrimeraQuin, SegundaQuin, Mensual } = paymentPeriod
+  const navegar = useNavigate()
+  const { PreNomina, Abierta } = statesPayroll
+  const { Semanal, PrimeraQuin, SegundaQuin, Mensual } = paymentPeriods
   // Estados del componente
+  const [isCreated, setIsCreated] = useState(false)
   const [departments, setDepartments] = useState([])
   const [perceptions, setPerceptions] = useState([])
   const [deductions, setDeductions] = useState([])
   const [payroll, setPayroll] = useState(false)
+  const [employees, setEmployees] = useState([])
   const [buttonDepart, setButtonDepart] = useState(false)
   const [buttonPercep, setButtonPercep] = useState(false)
   const [buttonDeduc, setButtonDeduc] = useState(false)
   const [date, setDate] = useState("")
   const [employeeDelete, setEmployeeDelete] = useState(null)
   const [modalDelete, setModalDelete] = useState(false)
+  const [modalDeletePayroll, setModalDeletePayroll] = useState(false)
   // Estados del formulario
   const [title, setTitle] = useState("")
-  const [periodSelected, setPeriodSelected] = useState("0")
+  const [paymentPeriod, setPaymentPeriod] = useState("0")
   const [arrayOfDepartments, setArrayOfDepartments] = useState([])
   const [arrayOfPerceps, setArrayOfPerceps] = useState([])
   const [arrayOfDeducs, setArrayOfDeducs] = useState([])
+  const [dateRange, setDateRange] = useState('')
   // console.log(departments);
 
   const { id: companyID, currency } = JSON.parse(
@@ -77,15 +84,48 @@ export function PrePayroll() {
     getDeduc()
   }, [])
 
-  const confirmDelete = () => {
+  const confirmDelete = async () => {
     // Aqui va ir el codigo con la peticion para eliminar un empleado de esta pre-nomina
-    const indexItem = payroll.findIndex(item => item.id === employeeDelete)
-    if (indexItem >= 0) {
-      const newPayroll = [...payroll]
-      newPayroll.splice(indexItem, 1)
-      setEmployeeDelete(null)
-      setPayroll(newPayroll)
-      setModalDelete(false)
+    const res = await deleteUserOfPayroll(token, payroll.id, employeeDelete)
+    if (res.message) {
+      // console.log(res.message)
+      const indexItem = payroll.findIndex(item => item.id === employeeDelete)
+      if (indexItem >= 0) {
+        const newPayroll = [...payroll]
+        newPayroll.splice(indexItem, 1)
+        setEmployeeDelete(null)
+        setPayroll(newPayroll)
+      }
+    } else {
+      console.error(res)
+    }
+  }
+
+  const confirmDeletePayroll = async () => {
+    // Aqui va ir el codigo con la peticion para eliminar un empleado de esta pre-nomina
+    const res = await deletePayroll(token, payroll.id, companyID)
+    if (res.message) {
+      setArrayOfDeducs([])
+      setArrayOfPerceps([])
+      setArrayOfDepartments([])
+      setEmployees([])
+      setIsCreated(false)
+      setPayroll(null)
+      setTitle('')
+      setPaymentPeriod('0')
+      setDate('')
+      setDateRange('')
+    } else {
+      console.error(res)
+    }
+  }
+
+  const confirmOpenPayroll = async () => {
+    const res = await updateStatePayroll(token, payroll.id, companyID, {state: Abierta})
+    if (res.message) {
+      navegar(`/nomina/${payroll.id}`)
+    } else {
+      console.error(res)
     }
   }
 
@@ -124,6 +164,22 @@ export function PrePayroll() {
     }
   }
 
+  const handleDateRange = (date, paymentPeriod) => {
+    {date ?
+      (paymentPeriod === Semanal
+        ? setDateRange(formatWeekRange(date))
+        : paymentPeriod === PrimeraQuin
+        ? setDateRange(formatRangeFirst(date))
+        : paymentPeriod === SegundaQuin
+        ? setDateRange(formatRangeSecond(date))
+        : setDateRange(formatMonth(date))
+      ) : (
+        setDateRange('')
+      )
+    }
+  }
+
+  // Aqui el codigo para generar la pre-nomina
   const handleSubmit = e => {
     e.preventDefault()
 
@@ -131,13 +187,17 @@ export function PrePayroll() {
 
     if (!title)
       return console.error("Debes colocarle un titulo a la pre-nomina")
-    if (periodSelected === "0")
+    if (paymentPeriod === "0")
       return console.error(
         "Debes seleccionar un periodo de pago de la pre-nomina"
       )
     if (!date)
       return console.error(
         "Debes seleccionar una fecha para el periodo de pago de la pre-nomina"
+      )
+    if (!dateRange)
+      return console.error(
+        "Error con el rango de fechas del periodo de pago de la pre-nomina"
       )
     if (arrayOfDepartments.length === 0)
       return console.error(
@@ -146,30 +206,35 @@ export function PrePayroll() {
 
     data = {
       title,
-      periodSelected: periodSelected === SegundaQuin ? "15" : periodSelected,
+      paymentPeriod: paymentPeriod,
+      dateRange,
       arrayOfDepartments,
       arrayOfPerceps,
-      arrayOfDeducs
+      arrayOfDeducs,
+      state: PreNomina
     }
 
-    console.log(data)
+    // console.log(data)
 
     fetch(
-      `http://localhost:3000/payroll/generate-payroll/${companyID}/${arrayOfDepartments[0]}`,
+      `http://localhost:3000/payroll/generate-payroll/${companyID}`,
       {
-        method: "GET",
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`
-        }
+        },
+        body: JSON.stringify(data)
       }
     )
       .then(response => response.json())
       .then(data => {
-        console.log("Success:", data)
+        // console.log("Success:", data)
         if (data.employees) {
-          console.log("Success:", data.employees)
-          setPayroll(data.employees)
+          console.log("Success:", data)
+          setPayroll(data.payroll)
+          setEmployees(data.employees)
+          setIsCreated(true)
         } else {
           setPayroll([])
           console.log("Error:", data.error)
@@ -180,20 +245,84 @@ export function PrePayroll() {
       })
   }
 
+  // Aqui el codigo para modificar los datos de la pre-nomina
+  const handleSubmitEdit = e => {
+    e.preventDefault()
+
+    let data = null
+
+    if (!title)
+      return console.error("Debes colocarle un titulo a la pre-nomina")
+    if (paymentPeriod === "0")
+      return console.error(
+        "Debes seleccionar un periodo de pago de la pre-nomina"
+      )
+    if (!date)
+      return console.error(
+        "Debes seleccionar una fecha para el periodo de pago de la pre-nomina"
+      )
+    if (!dateRange)
+      return console.error(
+        "Error con el rango de fechas del periodo de pago de la pre-nomina"
+      )
+    if (arrayOfDepartments.length === 0)
+      return console.error(
+        "Debes seleccionar un departamento de empleados para traer a la pre-nomina"
+      )
+
+    data = {
+      title,
+      paymentPeriod: paymentPeriod,
+      dateRange,
+      arrayOfDepartments,
+      arrayOfPerceps,
+      arrayOfDeducs
+    }
+
+    //Aqui debe ir el fetch para editar la informacion 
+    console.log("Edita la info")
+    // fetch(
+    //   `http://localhost:3000/payroll/generate-payroll/${companyID}`,
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Authorization: `Bearer ${token}`
+    //     },
+    //     body: JSON.stringify(data)
+    //   }
+    // )
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     // console.log("Success:", data)
+    //     if (data.employees) {
+    //       console.log("Success:", data)
+    //       setPayroll(data.employees)
+    //       setIsCreated(true)
+    //     } else {
+    //       setPayroll([])
+    //       console.log("Error:", data.error)
+    //     }
+    //   })
+    //   .catch(error => {
+    //     console.error("Error:", error)
+    //   })
+  }
+
   const handlePeriod = e => {
     const value = e.target.value
-    periodSelected === Semanal && value !== Semanal
-      ? setDate("")
-      : periodSelected !== Semanal && value === Semanal && setDate("")
-    setPeriodSelected(value)
+    if (paymentPeriod === Semanal && value !== Semanal) {
+      setDate("")
+      handleDateRange('', value)
+    } else if (paymentPeriod !== Semanal && value === Semanal) {
+      setDate("")
+      handleDateRange('', value)
+    }
+    setPaymentPeriod(value)
+    handleDateRange(date, value)
   }
 
-  const openPayroll = () => {
-    // Aqui va ir el codigo con la peticion para pasar la nomina de 'Pre-payroll a 'Abierta'
-    console.log("Nomina Abierta")
-  }
-
-  // console.log('Periodo ' + periodSelected)
+  // console.log('Periodo ' + paymentPeriod)
   // console.log('Fecha ' + date)
   // console.log('Departamentos ' + arrayOfDepartments)
   return (
@@ -209,13 +338,18 @@ export function PrePayroll() {
                 autoFocus
                 className="px-3 py-2 rounded-md border-b border-azulOscuro text-black placeholder:text-black outline-none w-2/4 font-semibold"
                 placeholder="Nombre de la nomina"
+                value={title}
                 onChange={e => setTitle(e.target.value)}
               />
               <button
                 className="bg-azulClaro px-10 py-2 rounded-md text-grisClaro outline-none"
-                onClick={handleSubmit}
+                onClick={e => {
+                  isCreated
+                    ? handleSubmitEdit(e)
+                    : handleSubmit(e)
+                }}
               >
-                Generar
+                {isCreated ? 'Modificar' : 'Generar'}
               </button>
             </div>
             <div className="flex justify-between items-start">
@@ -230,7 +364,7 @@ export function PrePayroll() {
                   name="period"
                   id="period"
                   className="bg-azulClaro px-3 py-2 rounded-md text-grisClaro outline-none w-60"
-                  value={periodSelected}
+                  value={paymentPeriod}
                   onChange={handlePeriod}
                 >
                   <option
@@ -245,28 +379,29 @@ export function PrePayroll() {
                   <option value={Mensual}>Mensual</option>
                 </select>
               </div>
-              {periodSelected !== "0" && (
+              {paymentPeriod !== "0" && (
                 <div className="flex flex-col gap-2">
                   <label
                     htmlFor="date"
                     className="font-semibold"
                   >
-                    {periodSelected === "7" ? "Semana" : "Mes"}
+                    {paymentPeriod === "7" ? "Semana" : "Mes"}
                   </label>
                   <input
-                    type={periodSelected === "7" ? "week" : "month"}
+                    type={paymentPeriod === "7" ? "week" : "month"}
                     name="date"
                     id="date"
                     className="bg-azulClaro px-3 py-2 rounded-md text-grisClaro outline-none w-60"
                     value={date}
                     onChange={e => {
-                      console.log(e.target.value)
+                      // console.log(e.target.value)
+                      handleDateRange(e.target.value, paymentPeriod)
                       setDate(e.target.value)
                     }}
                   />
                 </div>
               )}
-              {periodSelected !== "0" && (
+              {paymentPeriod !== "0" && (
                 <div
                   className={`${
                     date ? "opacity-100" : "opacity-0"
@@ -275,13 +410,8 @@ export function PrePayroll() {
                   <p className="font-semibold">Fecha</p>
                   <p className="bg-azulClaro px-3 py-2 rounded-md text-grisClaro w-60">
                     {date &&
-                      (periodSelected === Semanal
-                        ? formatWeekRange(date)
-                        : periodSelected === PrimeraQuin
-                        ? formatRangeFirst(date)
-                        : periodSelected === SegundaQuin
-                        ? formatRangeSecond(date)
-                        : formatMonth(date))}
+                      (dateRange)
+                    }
                   </p>
                 </div>
               )}
@@ -410,11 +540,11 @@ export function PrePayroll() {
             </div>
           </form>
         </section>
-        {payroll?.length === 0 ? (
+        {employees?.length === 0 ? (
           <strong className="text-xl">
             No hay empleados para asignar a esta nomina
           </strong>
-        ) : payroll?.length > 0 ? (
+        ) : employees?.length > 0 ? (
           <>
             <table className="bg-grisClaro rounded-md shadow-right-dark px-4 border-separate border-spacing-0 border-spacing-y-4 w-full border-2 border-grisOscuro">
               <thead className="px-5">
@@ -433,15 +563,13 @@ export function PrePayroll() {
                   <th className="p-4 text-lg text-start max-w-[8ch]">
                     Salario Bruto
                   </th>
-                  {/* <th className="p-4 text-lg text-start max-w-[8ch]">Total Percep.</th> */}
-                  {/* <th className="p-4 text-lg text-start max-w-[8ch]">Total Deduc.</th> */}
                   <th className="p-4 text-lg text-start max-w-[8ch]">
                     Salario Neto
                   </th>
                 </tr>
               </thead>
               <tbody className="px-5 relative">
-                {payroll.map(employee => (
+                {employees.map(employee => (
                   <tr
                     key={employee.id}
                     className="bg-grisOscuro hover:bg-blue-300"
@@ -458,12 +586,10 @@ export function PrePayroll() {
                       {formatTimeDifference(employee.startDate)}
                     </td>
                     <td className="p-4 text-lg">
-                      {employee.baseSalary} {currency}
+                      {employee.grossSalary} {currency}
                     </td>
-                    {/* <td className="p-4 text-lg">{employee.perceptions} Bs.</td> */}
-                    {/* <td className="p-4 text-lg">{employee.deductions} Bs.</td> */}
                     <td className="p-4 text-lg">
-                      {employee.baseSalary} {currency}
+                      {employee.netSalary} {currency}
                     </td>
                     <td className="relative p-4 text-lg rounded-r-2xl">
                       <input
@@ -502,21 +628,18 @@ export function PrePayroll() {
             </table>
             <section className="flex justify-between w-full px-5">
               <p className="bg-azulClaro px-5 py-3 rounded-md text-grisClaro font-semibold">
-                Total de empleados: {payroll.length}
+                Total de empleados: {employees.length}
               </p>
               <div className="flex gap-4">
                 <button
                   className="bg-azulClaro px-5 py-3 m-auto rounded-md placeholder-grisClaro text-grisClaro outline-none font-semibold"
-                  onClick={() => {
-                    setArrayOfDepartments("0")
-                    setPayroll(null)
-                  }}
+                  onClick={() => {setModalDeletePayroll(true)}}
                 >
                   Cancelar Pre-Nomina
                 </button>
                 <button
                   className="bg-azulClaro px-5 py-3 m-auto rounded-md placeholder-grisClaro text-grisClaro outline-none font-semibold"
-                  onClick={() => openPayroll()}
+                  onClick={() => confirmOpenPayroll()}
                 >
                   Abrir Nomina
                 </button>
@@ -530,6 +653,12 @@ export function PrePayroll() {
           <ModalDelete
             peticion={confirmDelete}
             setStateModal={setModalDelete}
+          />
+        )}
+        {modalDeletePayroll && (
+          <ModalDelete
+            peticion={confirmDeletePayroll}
+            setStateModal={setModalDeletePayroll}
           />
         )}
       </main>
